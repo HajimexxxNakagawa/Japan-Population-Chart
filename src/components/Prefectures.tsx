7515cf586d1701f5b6da1ec56413c56df5a9d976
Hajime Nakagawa <navyblue0914@icloud.com>
2021-10-22 10:47:01 +0900

    CheckBoxListを作成

--- src/components/Prefectures.tsx
+++ src/components/Prefectures.tsx
@@ -2,6 +2,8 @@ import React, { VFC } from 'react'
 
 import { useResas } from '@/hooks/useResas'
 
+import styles from '@/styles/CheckBoxList.module.css'
+
 interface Prefecture {
   prefCode: number
   prefName: string
@@ -11,10 +13,19 @@ export const Prefectures: VFC = () => {
   const { result } = useResas<Prefecture[]>('api/v1/prefectures')
 
   return (
-    <>
+    <div className={styles.list}>
       {result?.map(({ prefCode, prefName }) => {
-        return <p key={prefCode}>{prefName}</p>
+        return <CheckBox key={prefCode} label={prefName} />
       })}
-    </>
+    </div>
+  )
+}
+
+const CheckBox = ({ label }: { label: string }) => {
+  return (
+    <label htmlFor={label}>
+      <input id={label} type="checkbox" name={label} />
+      {label}
+    </label>
   )
 }
